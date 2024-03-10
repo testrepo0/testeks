8d2b70b091aff342d24e263b59acaa72d8f41090
patongit <90884766+patgit12@users.noreply.github.com>
2024-03-10 15:22:50 -0400

    updating

--- data_remote.tf
+++ data_remote.tf
@@ -1,6 +1,15 @@
-data "terraform_remote_state" "network" { 
+/*data "terraform_remote_state" "network" { 
   backend = "local"
   config = {
     path = "../EKS_module/vpcseries/terraform.tfstate"
+  }
+} */
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "modulebuck"
+    key = "statefiles/terraform.tfstate"
+    region = "us-east-1"
   }
 }
